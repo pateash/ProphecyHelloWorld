36521bb5a8b201ba4ed5a3cf796bd941b6218aa8
ashishpatel0720@gmail.com <ashishpatel0720@gmail.com>
2024-12-10 04:25:07 +0000

    Adjust account length calculation in customer_orders_demo pipeline by increasing offset to 404 days

--- pipelines/customer_orders_demo/code/customer_orders_demo/graph/Sum_Amounts.py
+++ pipelines/customer_orders_demo/code/customer_orders_demo/graph/Sum_Amounts.py
@@ -12,7 +12,7 @@ def Sum_Amounts(spark: SparkSession, in0: DataFrame) -> DataFrame:
     return df1.agg(
         count(col("order_id")).alias("orders"), 
         sum(col("amount")).alias("amounts"), 
-        (first(col("account_length_days")) + lit(101)).alias("account_length_days"), 
+        (first(col("account_length_days")) + lit(404)).alias("account_length_days"), 
         first(col("first_name")).alias("first_name"), 
         first(col("last_name")).alias("last_name")
     )
